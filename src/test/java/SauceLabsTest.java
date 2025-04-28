7db3c49ed510e66713eb137bdbf369d9078a1590
luisenrique94 <luisenriquechavezporroa@gmail.com>
2025-04-28 17:24:29 -0500

    feat: se agrego captura de pantalla y reporte xml

--- src/test/java/SauceLabsTest.java
+++ src/test/java/SauceLabsTest.java
@@ -10,12 +10,21 @@ import org.openqa.selenium.WebElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SauceLabsTest {
         private WebDriver driver;
 
         @BeforeEach
         public void setUp() {
+            cleanFolder("./screenshots");
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
         }
@@ -25,6 +34,7 @@ public class SauceLabsTest {
             // Abrir Google
             driver.manage().window().maximize();
             driver.get("https://www.saucedemo.com/");
+            takeScreenshot("testLoginSauceLabs_success_web");
 
             // Encontrar el campo de userName
             WebElement userName = driver.findElement(By.name("user-name"));
@@ -37,6 +47,7 @@ public class SauceLabsTest {
             //Encontrar boton login
             WebElement btnLogin = driver.findElement(By.id("login-button"));
             // Ingresar login
+            takeScreenshot("testLoginSauceLabs_success_datos_ingresados");
             btnLogin.click();
 
 
@@ -48,11 +59,35 @@ public class SauceLabsTest {
                 System.out.println("el texto encontrado es:" +result.getText());
             }
 
+            takeScreenshot("testLoginSauceLabs_success");
             // Verificar que aparecen resultados
            // Assertions.assertTrue(driver.findElements(By.cssSelector("h3")).size() > 0);
         }
 
-        @AfterEach
+    public void takeScreenshot(String testName) {
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        try {
+            FileUtils.copyFile(screenshot, new File("./screenshots/" + testName + "_" + timestamp + ".png"));
+            System.out.println("Captura de pantalla guardada: " + testName + "_" + timestamp + ".png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void cleanFolder(String folderPath) {
+        File folder = new File(folderPath);
+        if (folder.exists()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    f.delete();
+                }
+            }
+        }
+    }
+
+
+    @AfterEach
         public void tearDown() {
             if (driver != null) {
                 driver.quit();
